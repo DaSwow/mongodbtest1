9317a4492f2ca6be39d778a5e23479e0e4328bf9
DaSwow <carls09995@hotmail.com>
2020-05-12 10:00:29 -0700

    Mongo

--- src/mongodbtest1/Mongodbtest1.java
+++ src/mongodbtest1/Mongodbtest1.java
@@ -1,14 +1,14 @@
 package mongodbtest1;
 
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import java.util.Arrays;
-import java.util.logging.Logger;
+import org.bson.BsonDocument;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 /**
  *
@@ -34,38 +34,39 @@ public class Mongodbtest1 {
         //         .applyConnectionString(connectionString)
         //         .codecRegistry(codecRegistry)
         //        .build();
-   
-        
         try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017/bddmongo")) {
             MongoDatabase db = mongoClient.getDatabase("bddmongo");
-            
-            
+
             MongoCollection<Document> restaurantes = db.getCollection("restaurantes", Document.class);
 
             Document document1 = new Document()
-                    .append("nombre", "canvas")
-                    .append("categorias", new Document().append("", 28.0).append("w", 35.5).append("uom", "cm"))
-                    .append("estrellas", "5")
-                    .append("size", new Document().append("h", 28.0).append("w", 35.5).append("uom", "cm"));
+                    .append("nombre", "Sushilito")
+                    .append("categorias", new Document().append("Especialidad", "Sushi").append("Nacionalidad", "Japonesa"))
+                    .append("estrellas", 3);
             System.out.println(document1.toJson());
 
             Document document2 = new Document()
-                    .append("item", "journal")
-                    .append("qty", 25)
-                    .append("tags", Arrays.asList("blank", "red"))
-                    .append("size", new Document().append("h", 14.0).append("w", 21.0).append("uom", "cm"));
+                    .append("nombre", "El Padrino")
+                    .append("categorias", new Document().append("Especialidad", "Pizza").append("Nacionalidad", "Italiana"))
+                    .append("estrellas", 5);
             System.out.println(document2.toJson());
 
             Document document3 = new Document()
-                    .append("item", "mousepad")
-                    .append("qty", 25)
-                    .append("tags", Arrays.asList("get", "blue"))
-                    .append("size", new Document().append("h", 19.0).append("w", 22.85).append("uom", "in"));
+                    .append("nombre", "El Mexiantojo")
+                    .append("categorias", new Document().append("Especialidad", "Enchiladas").append("Nacionalidad", "Mexicana"))
+                    .append("estrellas", 4);
             System.out.println(document3.toJson());
 
-           
-
             restaurantes.insertMany(Arrays.asList(document1, document2, document3));
+
+            Bson bson =  Filters.and(
+                        Filters.gt("estrellas", 4),
+                        Filters.eq("canvas", "journal"));
+            
+            BsonDocument bsonDocument = bson.toBsonDocument(Class.forName(Bson.class.getCanonicalName()));
+
+            System.out.print(bsonDocument.toString()); //{ "inbox" : { "$in" : inboxes }, "status" : status }
+
         }
 
         //  MongoCollection<Document> collection = database.getCollection("inventory");
