93254cdd7a6a2a26f53a6df48249bd8bee3f61bb
DaSwow <carls09995@hotmail.com>
2020-05-12 13:12:53 -0700

    Mongo

--- src/mongodbtest1/Mongodbtest1.java
+++ src/mongodbtest1/Mongodbtest1.java
@@ -1,15 +1,35 @@
 package mongodbtest1;
 
+import com.mongodb.AggregationOutput;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Accumulators;
 import com.mongodb.client.model.Aggregates;
+import static com.mongodb.client.model.Aggregates.match;
 import com.mongodb.client.model.Filters;
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.expr;
+import static com.mongodb.client.model.Filters.lt;
+import static com.mongodb.client.model.Filters.regex;
+import static com.mongodb.client.model.Filters.type;
+import static com.mongodb.client.model.Projections.fields;
+import com.mongodb.client.result.DeleteResult;
+import static java.lang.Math.log;
+import java.util.ArrayList;
 import java.util.Arrays;
-import org.bson.BsonDocument;
+import static java.util.Collections.list;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
@@ -45,68 +65,95 @@ public class Mongodbtest1 {
             Document document1 = new Document()
                     .append("nombre", "Sushilito")
                     .append("categorias", new Document().append("Especialidad", "Sushi").append("Nacionalidad", "Japonesa"))
-                    .append("estrellas", 3);
-            System.out.println(document1.toJson());
+                    .append("estrellas", 5);
+            //System.out.println(document1.toJson());
 
             Document document2 = new Document()
                     .append("nombre", "El Padrino")
                     .append("categorias", new Document().append("Especialidad", "Pizza").append("Nacionalidad", "Italiana"))
-                    .append("estrellas", 5);
-            System.out.println(document2.toJson());
+                    .append("estrellas", 4);
+            //System.out.println(document2.toJson());
 
             Document document3 = new Document()
                     .append("nombre", "El Mexiantojo")
                     .append("categorias", new Document().append("Especialidad", "Enchiladas").append("Nacionalidad", "Mexicana"))
-                    .append("estrellas", 4);
-            System.out.println(document3.toJson());
+                    .append("estrellas", 3);
+            //System.out.println(document3.toJson());
 
             restaurantes.insertMany(Arrays.asList(document1, document2, document3));
-
-            AggregateIterable<Document> documents = restaurantes.aggregate(Arrays.asList(
+//
+//
+//
+            System.out.println("Restaurantes con mas de 4 estrellas.");
+            AggregateIterable<Document> filtrado1 = restaurantes.aggregate(Arrays.asList(
                     // Java equivalent of the $match stage
                     Aggregates.match(Filters.gt("estrellas", 4))
             ));
 
-            for (Document document : documents) {
+            for (Document document : filtrado1) {
                 System.out.println(document.toJson());
+            }
+//
+//
+//
+            System.out.println("Restaurantes con la cagerogia Pizza");
+
+            FindIterable<Document> filtrado2 = restaurantes.find(eq("categorias.Especialidad", "Pizza"));
+
+            for (Document document : filtrado2) {
+                System.out.println(document.toJson());
+            }
+//
+//
+//
+            System.out.println("Restaurantes cuyo nombre incluya Sushi");
+            Pattern pattern = Pattern.compile(".*" + Pattern.quote("Sushi") + ".*", Pattern.CASE_INSENSITIVE);
+            FindIterable<Document> filtrado3 = restaurantes.find(regex("nombre", pattern));
+
+            for (Document document : filtrado3) {
+                System.out.println(document.toJson());
+            }
+//
+//
+//
+            System.out.println("Agregando una categoria a Sushilito");
+            DBObject listItem = new BasicDBObject("categorias.Chefs", "Sanmoto Gorozaemon");
+            restaurantes.updateOne(eq("nombre", "Sushilito"), new Document().append("$set", listItem));
+
+            restaurantes = db.getCollection("restaurantes", Document.class);
+            FindIterable<Document> filtrado4 = restaurantes.find(regex("nombre", pattern));
+
+            for (Document document : filtrado4) {
+                System.out.println(document.toJson());
+            }
+//
+//
+//
+            System.out.println("Eliminando un restaurante por su identificador");
+            BasicDBObject documentoABorrar = new BasicDBObject();
+            documentoABorrar.put("nombre", "Sushilito");
+            restaurantes.deleteOne(documentoABorrar);
+
+            try (MongoCursor<Document> cursor = restaurantes.find().iterator()) {
+                while (cursor.hasNext()) {
+                    System.out.println(cursor.next());
+                }
+            }
+//
+//
+//     
+            System.out.println("Eliminando restaurantes con 3 estrellas o menos.");
+
+            restaurantes.deleteMany(lt("estrellas", 3));
+            restaurantes.deleteMany(eq("estrellas", 3));
+            
+            try (MongoCursor<Document> cursor = restaurantes.find().iterator()) {
+                while (cursor.hasNext()) {
+                    System.out.println(cursor.next());
+                }
             }
 
         }
-
-        //  MongoCollection<Document> collection = database.getCollection("inventory");
-//        Document document1 = new Document()
-//                .append("item", "canvas")
-//                .append("qty", 100)
-//                .append("tags", Arrays.asList("cotton"))
-//                .append("size", new Document().append("h", 28.0).append("w", 35.5).append("uom", "cm"));
-//        System.out.println(document1.toJson());
-//
-//        Document document2 = new Document()
-//                .append("item", "journal")
-//                .append("qty", 25)
-//                .append("tags", Arrays.asList("blank", "red"))
-//                .append("size", new Document().append("h", 14.0).append("w", 21.0).append("uom", "cm"));
-//
-//        Document document3 = new Document()
-//                .append("item", "mousepad")
-//                .append("qty", 25)
-//                .append("tags", Arrays.asList("get", "blue"))
-//                .append("size", new Document().append("h", 19.0).append("w", 22.85).append("uom", "in"));
-//
-////        collection.insertMany(Arrays.asList(document1, document2, document3));
-//      //  System.out.println(collection.countDocuments());
-//
-////        for (Document doc : collection.find()) {
-////            System.out.println(doc.toJson());
-////        }
-////        FindIterable<Document> documents=collection.find();
-//        Filters.or(
-//                Filters.eq("status", "A"),
-//                Filters.and(
-//                        Filters.gt("qty", 30),
-//                        Filters.eq("canvas", "journal"))
-//        );
-//
     }
 
 }
